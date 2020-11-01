7b4b25937095287ec8716014083c69f368227ae6
amitSth123 <amitshrestha40@gmail.com>
2020-11-01 14:31:01 +0545

    transfer

---
+++ src/app/Service/Billing/purchase.service.ts
@@ -0,0 +1,81 @@
+﻿
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+
+@Injectable()
+
+export class PurchaseService {
+
+    constructor(private _http: HttpClient) { }
+
+
+    get(url: string): Observable<any> {
+        return this._http.get(url).pipe(
+
+            catchError(this.handleError));
+    }
+
+    post(url: string, model: any): Observable<any> {
+        let body = JSON.stringify(model);
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        let options = ({ headers: headers });
+        return this._http.post(url, body, options).pipe(
+
+            catchError(this.handleError));
+    }
+
+    put(url: string, id: number, model: any): Observable<any> {
+        let body = JSON.stringify(model);
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        let options = ({ headers: headers });
+        return this._http.put(url + id, body, options).pipe(
+
+            catchError(this.handleError));
+    }
+
+    delete(url: string, id: number): Observable<any> {
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        let options = ({ headers: headers });
+        return this._http.delete(url + id, options).pipe(
+
+            catchError(this.handleError));
+    }
+
+
+    getInventoryItems() {
+        return this._http.get("/api/InventoryItemAPI/get")
+            .pipe(
+                catchError(this.handleError)
+            )
+    }
+
+    getSalesItems() {
+        return this._http.get("/api/MenuCategoryItemAPI")
+            .pipe(
+                catchError(this.handleError)
+            )
+    }
+
+    getAccounts() {
+
+        return this._http.get("/api/AccountAPI/get")
+            .pipe(
+                catchError(this.handleError)
+            )
+    } 
+
+    private handleError(error: HttpErrorResponse) {
+        return throwError(error.message || 'Server error');
+    }
+
+
+
+}
+
