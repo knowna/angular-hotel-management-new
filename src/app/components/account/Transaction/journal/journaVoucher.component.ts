e2f6dc7e7269043e78f7f1701c01926bd492d4d4
amitSth123 <amitshrestha40@gmail.com>
2020-12-16 07:32:39 +0545

    popup size increased

--- src/app/components/account/Transaction/journal/journaVoucher.component.ts
+++ src/app/components/account/Transaction/journal/journaVoucher.component.ts
@@ -260,7 +260,7 @@ export class JournalVouchercomponent implements OnInit {
         this.modalRef = this.modalService.show(this.TemplateRef, {
             backdrop: 'static',
             keyboard: false,
-            class: 'modal-lg'
+            class: 'modal-xl'
         });
     }
 
@@ -322,7 +322,7 @@ export class JournalVouchercomponent implements OnInit {
                 this.modalRef = this.modalService.show(this.TemplateRef, {
                     backdrop: 'static',
                     keyboard: false,
-                    class: 'modal-lg'
+                    class: 'modal-xl'
                 });
             },
                 error => this.msg = <any>error);
@@ -374,7 +374,7 @@ export class JournalVouchercomponent implements OnInit {
                 this.modalRef = this.modalService.show(this.TemplateRef, {
                     backdrop: 'static',
                     keyboard: false,
-                    class: 'modal-lg'
+                    class: 'modal-xl'
                 });
             });
     }
