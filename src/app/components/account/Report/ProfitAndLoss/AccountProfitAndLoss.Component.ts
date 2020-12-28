ee746aab77c3e43d113a201e52ab132f7ed90e08
amitSth123 <amitshrestha40@gmail.com>
2020-12-28 08:19:56 +0545

    pdf downloading in the process => sales book,profitloss,ledger

--- src/app/components/account/Report/ProfitAndLoss/AccountProfitAndLoss.Component.ts
+++ src/app/components/account/Report/ProfitAndLoss/AccountProfitAndLoss.Component.ts
@@ -9,6 +9,11 @@ import { ProfitAndLoss } from 'src/app/Model/ProfitAndLoss';
 import { JournalVoucherService } from 'src/app/Service/journalVoucher.service';
 import { Global } from 'src/app/Shared/global';
 
+import * as XLSX from 'xlsx';
+//generating pdf
+import * as jsPDF from 'jspdf'
+import 'jspdf-autotable';
+
 @Component({
     templateUrl: './AccountProfitAndLoss.Component.html'
 })
@@ -24,6 +29,9 @@ export class AccountProfitAndLossComponent implements OnInit {
     modalBtnTitle: string;
     modalRef: BsModalRef;
     private formSubmitAttempt: boolean;
+
+    toExportFileName: string = 'Profit and Loss-' + this.date.transform(new Date, "yyyy-MM-dd") + '.xlsx';
+    toPdfFileName: string = 'Profit and Loss-' + this.date.transform(new Date, "yyyy-MM-dd") + '.pdf';
 
     constructor(
         private _ProfitAndLossService: JournalVoucherService,
@@ -45,36 +53,97 @@ export class AccountProfitAndLossComponent implements OnInit {
                 error => this.msg = <any>error);
     }
 
-    exportTableToExcel(tableID, filename = '') {
-        var downloadLink;
-        var dataType = 'application/vnd.ms-excel';
-        var clonedtable = $('#' + tableID);
-        var clonedHtml = clonedtable.clone();
-        $(clonedtable).find('.export-no-display').remove();
-        var tableSelect = document.getElementById(tableID);
-        var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-        $('#' + tableID).html(clonedHtml.html());
+    exportTableToPdf() {
+        var doc = new jsPDF("p", "mm", "a4");
+        var rows = [];
+        let sn = 1;
 
-        // Specify file name
-        filename = filename ? filename + '.xls' : 'Profit and Loss of ' + this.date.transform(new Date, 'dd-MM-yyyy') + '.xls';
+        rows.push(['Particular','Amount (Rs.)']);
 
-        // Create download link element
-        downloadLink = document.createElement("a");
+        this.profitandloss.forEach(pl => {
+            var tempProfitLoss = [
+                pl.Name,
+                pl.Amount !=0  ?  pl.Amount.toFixed(2) : ''
+            ];  
 
-        document.body.appendChild(downloadLink);
+            rows.push(tempProfitLoss);
+        });
 
-        if (navigator.msSaveOrOpenBlob) {
-            var blob = new Blob(['\ufeff', tableHTML], { type: dataType });
-            navigator.msSaveOrOpenBlob(blob, filename);
-        } else {
-            // Create a link to the file
-            downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+        doc.setFontSize(14);
+        doc.text(80,20, `${this.company?.NameEnglish}`);
+        doc.text(87,30,'Profit and Loss');
+        // doc.text(80,40,'Account : ' + account.Name);
 
-            // Setting the file name
-            downloadLink.download = filename;
+        doc.autoTable({
+            margin: {left: 10,bottom:20},
+            setFontSize: 14,
+    
+            //for next page 
+            startY: doc.pageCount > 1? doc.autoTableEndPosY() + 20 : 40,
+            rowPageBreak: 'avoid',
+            body: rows,
+            bodyStyles: {
+                fontSize: 9,
+            },
+            // customize table header and rows format
+            theme: 'striped'
+        });
 
-            //triggering the function
-            downloadLink.click();
+        const pages = doc.internal.getNumberOfPages();
+        const pageWidth = doc.internal.pageSize.width;  //Optional
+        const pageHeight = doc.internal.pageSize.height;  //Optional
+        doc.setFontSize(10);  //Optional
+
+        for(let j = 1; j < pages + 1 ; j++) {
+            let horizontalPos = pageWidth / 2;  //Can be fixed number
+            let verticalPos = pageHeight - 10;  //Can be fixed number
+            doc.setPage(j);
+            doc.text(`${j} of ${pages}`, horizontalPos, verticalPos, {align: 'center' }); //Optional text styling});
         }
+
+        doc.save(this.toPdfFileName);
+
+    }
+
+    exportTableToExcel(tableID, filename = '') {
+        // var downloadLink;
+        // var dataType = 'application/vnd.ms-excel';
+        // var clonedtable = $('#' + tableID);
+        // var clonedHtml = clonedtable.clone();
+        // $(clonedtable).find('.export-no-display').remove();
+        // var tableSelect = document.getElementById(tableID);
+        // var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+        // $('#' + tableID).html(clonedHtml.html());
+
+        // // Specify file name
+        // filename = filename ? filename + '.xls' : 'Profit and Loss of ' + this.date.transform(new Date, 'dd-MM-yyyy') + '.xls';
+
+        // // Create download link element
+        // downloadLink = document.createElement("a");
+
+        // document.body.appendChild(downloadLink);
+
+        // if (navigator.msSaveOrOpenBlob) {
+        //     var blob = new Blob(['\ufeff', tableHTML], { type: dataType });
+        //     navigator.msSaveOrOpenBlob(blob, filename);
+        // } else {
+        //     // Create a link to the file
+        //     downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+
+        //     // Setting the file name
+        //     downloadLink.download = filename;
+
+        //     //triggering the function
+        //     downloadLink.click();
+        // }
+        let element = document.getElementById(tableID); 
+        const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+
+        /* generate workbook and add the worksheet */
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+        /* save to file */
+        XLSX.writeFile(wb, this.toExportFileName);
     }
 }
--- src/app/components/account/master/account-type/account-type.component.ts
+++ src/app/components/account/master/account-type/account-type.component.ts
@@ -9,6 +9,9 @@ import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { DatePipe } from '@angular/common';
 import { AccountTypeService } from '../master-ledger/services/account-type.service';
 
+import * as XLSX from 'xlsx';
+
+
 @Component({
     templateUrl: './account-type.component.html'
 })
@@ -31,6 +34,9 @@ export class AccountTypeComponent implements OnInit {
     private buttonDisabled: boolean;
 
     searchKeyword = '';
+
+    toExportFileName: string = 'Group Ledger-' + this.date.transform(new Date, "yyyy-MM-dd") + '.xlsx';
+    toPdfFileName: string = 'Group Ledger-' + this.date.transform(new Date, "yyyy-MM-dd") + '.pdf';
 
     constructor(private fb: FormBuilder, private accTypeService: AccountTypeService, private modalService: BsModalService, private date: DatePipe) {
         // this.accTypeService.getaccounttypes().subscribe(data => { this.accountTypes = data });
@@ -78,36 +84,47 @@ export class AccountTypeComponent implements OnInit {
     }
 
     exportTableToExcel(tableID, filename = '') {
-        var downloadLink;
-        var dataType = 'application/vnd.ms-excel';
-        var clonedtable = $('#' + tableID);
-        var clonedHtml = clonedtable.clone();
-        $(clonedtable).find('.export-no-display').remove();
-        var tableSelect = document.getElementById(tableID);
-        var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-        $('#' + tableID).html(clonedHtml.html());
+        // var downloadLink;
+        // var dataType = 'application/vnd.ms-excel';
+        // var clonedtable = $('#' + tableID);
+        // var clonedHtml = clonedtable.clone();
+        // $(clonedtable).find('.export-no-display').remove();
+        // var tableSelect = document.getElementById(tableID);
+        // var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+        // $('#' + tableID).html(clonedHtml.html());
 
-        // Specify file name
-        filename = filename ? filename + '.xls' : 'Account Type of ' + this.date.transform(new Date, 'dd-MM-yyyy') + '.xls';
+        // // Specify file name
+        // filename = filename ? filename + '.xls' : 'Account Type of ' + this.date.transform(new Date, 'dd-MM-yyyy') + '.xls';
 
-        // Create download link element
-        downloadLink = document.createElement("a");
+        // // Create download link element
+        // downloadLink = document.createElement("a");
 
-        document.body.appendChild(downloadLink);
+        // document.body.appendChild(downloadLink);
 
-        if (navigator.msSaveOrOpenBlob) {
-            var blob = new Blob(['\ufeff', tableHTML], { type: dataType });
-            navigator.msSaveOrOpenBlob(blob, filename);
-        } else {
-            // Create a link to the file
-            downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+        // if (navigator.msSaveOrOpenBlob) {
+        //     var blob = new Blob(['\ufeff', tableHTML], { type: dataType });
+        //     navigator.msSaveOrOpenBlob(blob, filename);
+        // } else {
+        //     // Create a link to the file
+        //     downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
 
-            // Setting the file name
-            downloadLink.download = filename;
+        //     // Setting the file name
+        //     downloadLink.download = filename;
 
-            //triggering the function
-            downloadLink.click();
-        }
+        //     //triggering the function
+        //     downloadLink.click();
+        // }
+        let element = document.getElementById('GroupLedgerTable'); 
+        const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+        ws['!cols'] = [];
+        ws['!cols'][3] = { hidden: true };
+
+        /* generate workbook and add the worksheet */
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+        /* save to file */
+        XLSX.writeFile(wb, this.toExportFileName);
     }
 
 
